321914d8aa0b57c6178b297b09901cd82d3a34a5
online <onlinegrave50@gmail.com>
2023-03-15 16:54:41 +0800

    CoolBudy1 FavSongs

---
+++ src/com/code_help/CoolBudy1/FavSongs.java
@@ -0,0 +1,44 @@
+package com.code_help.CoolBudy1;
+
+/**
+ * Create a Java program in BlueJ called FavSongs.
+ *
+ * Include your main method.
+ *
+ * Create three additional methods. Each method will print a different “song name”, (indented) a verse, and artist name.
+ *
+ * The first method should print your first favorite song name, favorite verse, and artist of your choice.
+ *
+ * The second method should print your second favorite song name,  verse, and artist of your choice.
+ *
+ * The third method should print your third favorite song name, verse,  and artist of your choice.
+ *
+ * Print each to the console. Include blank lines between each individual method.
+ */
+public class FavSongs {
+    public static void main(String[] args) {
+        printFirstFav();
+        System.out.println();
+        printSecondFav();
+        System.out.println();
+        printThirdFav();
+    }
+
+    public static void printFirstFav() {
+        System.out.println("Song 1");
+        System.out.println("Verse 1");
+        System.out.println("Artist 1");
+    }
+
+    public static void printSecondFav() {
+        System.out.println("Song 1");
+        System.out.println("Verse 1");
+        System.out.println("Artist 1");
+    }
+
+    public static void printThirdFav() {
+        System.out.println("Song 1");
+        System.out.println("Verse 1");
+        System.out.println("Artist 1");
+    }
+}
